23eeee8910dc2f3cd01f6c62bb8be43356207afc
Adrian Snela <grunaldi.mail@gmail.com>
2018-01-23 18:34:56 +0100

    Initial second

---
+++ src/main/java/Snela/com/sda/git/task1/Calculator.java
@@ -0,0 +1,16 @@
+package Snela.com.sda.git.task1;
+
+public class Calculator {
+        public float plus(float a, float b) {
+            return a + b;
+        }
+        public float minus(float a, float b) {
+            return a - b;
+        }
+        public float times(float a, float b) {
+            return a * b;
+        }
+        public float by(float a, float b) {
+            return a / b;
+        }
+}
